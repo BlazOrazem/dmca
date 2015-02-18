0225d0ddafaba12a9bc301f082f227af3fc48fed
Blaž Oražem <blaz.orazem@spletna-postaja.com>
2015-02-18 09:48:28 +0100

    Lesson 3

---
+++ app/Http/routes.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * The Home Page.
+ */
+Route::get('/', 'PagesController@home');
+
+/**
+ * Authentication.
+ */
+Route::controllers([
+	'auth' => 'Auth\AuthController',
+	'password' => 'Auth\PasswordController',
+]);
---
+++ resources/views/pages/home.blade.php
@@ -0,0 +1,5 @@
+@extends('app')
+
+@section('content')
+    The home page.
+@stop
